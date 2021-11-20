b97599a2a82f85e4c3ba2df6e61ada469c6dff58
Krokodili <luga.geri@gmail.com>
2021-11-20 17:27:09 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,62 @@
+const tiranaLondon = 2000;
+const tiranaRome = 500;
+const tiranaPrishtina = 200;
+const LondonRome = 1200;
+const LondonPrishtina = 2500;
+const RomePrishtina = 700;
+
+
+function findDistanceInKmBetweenTwoStations (start,destination){
+ 
+     return start.toLowerCase()===`tirana` && destination.toLowerCase() ===`prishtina`? tiranaPrishtina:
+     start.toLowerCase().replaceAll(' ','')===`tirana` && destination.toLowerCase().replaceAll(' ','') ===`london`?tiranaLondon:
+     start.toLowerCase().replaceAll(' ','')===`tirana` && destination.toLowerCase().replaceAll(' ','') ===`rome`?tiranaRome:
+     start.toLowerCase().replaceAll(' ','')===`tirana` && destination.toLowerCase().replaceAll(' ','') ===`tirana`?0:
+     start.toLowerCase().replaceAll(' ','')===`prishtina` && destination.toLowerCase().replaceAll(' ','') ===`tirana`?tiranaPrishtina:
+     start.toLowerCase().replaceAll(' ','')===`prishtina` && destination.toLowerCase().replaceAll(' ','') ===`London`?LondonPrishtina:
+     start.toLowerCase().replaceAll(' ','')===`prishtina` && destination.toLowerCase().replaceAll(' ','') ===`rome`?RomePrishtina:
+     start.toLowerCase().replaceAll(' ','')===`prishtina` && destination.toLowerCase().replaceAll(' ','') ===`prishtina`?0:
+     start.toLowerCase().replaceAll(' ','')===`london` && destination.toLowerCase().replaceAll(' ','') ===`tirana`?tiranaLondon:
+     start.toLowerCase().replaceAll(' ','')===`london` && destination.toLowerCase().replaceAll(' ','') ===`london`?0:
+     start.toLowerCase().replaceAll(' ','')===`london` && destination.toLowerCase().replaceAll(' ','') ===`rome`?LondonRome:
+     start.toLowerCase().replaceAll(' ','')===`london` && destination.toLowerCase().replaceAll(' ','') ===`prishtina`?LondonPrishtina:
+     start.toLowerCase().replaceAll(' ','')===`rome` && destination.toLowerCase().replaceAll(' ','') ===`tirana`?tiranaRome:
+     start.toLowerCase().replaceAll(' ','')===`rome` && destination.toLowerCase().replaceAll(' ','') ===`london`?LondonRome:
+     start.toLowerCase().replaceAll(' ','')===`rome` && destination.toLowerCase().replaceAll(' ','') ===`rome`?0:
+     start.toLowerCase().replaceAll(' ','')===`rome` && destination.toLowerCase().replaceAll(' ','') ===`prishtina`?RomePrishtina:
+     alert(`Can't you read?`);
+    
+     }
+
+     function alertPriceToPay(age,stations,start,destination){
+        if(age<0 || Number.isNaN(age)){
+           alert('Wrong age')
+           
+        }else if(!stations.includes(start.toLowerCase())){
+           alert('Wrong station')
+       } else if(!stations.includes(destination.toLowerCase())){
+           alert('Wrong station')
+       }else{
+        alert(`You have to pay £${age<18?normalPrice*0.2:age>65?normalPrice*0.4:normalPrice} for the ride.`);
+       }
+        }
+
+
+ let  age = Number(prompt("How old are you?"))
+ 
+ let  start = prompt(`
+ Where from?
+ Stations to choose from: "Tirana, London, Rome, Prishtina"
+ `);
+ let  destination = prompt(`Where to?
+ Stations to choose from: "Tirana, London, Rome, Prishtina"`);
+ const pricePerKm = 0.21;
+
+const km = findDistanceInKmBetweenTwoStations(start,destination)
+// debugger
+ let normalPrice = km*pricePerKm;
+ let stations=['tirana','rome','london','prishtina']
+
+
+
+ alertPriceToPay(age,stations,start,destination)
